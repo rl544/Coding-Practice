201bc6d62d440847dbdff8012dddbc695a068639
rl544 <111494264+rl544@users.noreply.github.com>
2022-12-01 16:46:06 +0900

    updated at 2022. 12. 1. 오후 4:46:06

--- programmers/명예의 전당 (1)/solution.java
+++ programmers/명예의 전당 (1)/solution.java
@@ -1,27 +1,17 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/138477
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
+
 class Solution {
     public int[] solution(int k, int[] score) {
-        int[] answer2 = {};
-        ArrayList <Integer> answer = new ArrayList();
-        ArrayList <Integer> hall = new ArrayList();
-        for(int i = 0; i < score.length; i++){
-            if(i > k-1){
-                if(score[i] > hall.get(0)){
-                    hall.add(score[i]);
-                    hall.remove(hall.get(0));
-                }
-            }
-            else hall.add(score[i]);
-            Collections.sort(hall);
-            answer.add(hall.get(0));
-        }
-        answer2 = new int[answer.size()];
-        for (int i = 0; i < answer.size(); i++) {
-            answer2[i] = answer.get(i);
-        }
-        return answer2;
+        int l = score.length;
+        int[] answer = new int[l];
+        PriorityQueue<Integer> que1 = new PriorityQueue<>();
+        for (int i = 0; i < l; i++){
+            que1.add(score[i]);
+            if(que1.size() > k) que1.poll();
+            answer[i] = que1.peek();
+        }        
+        return answer;
     }
 }
