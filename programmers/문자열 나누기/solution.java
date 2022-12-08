8df9fb15fda54a787570c24fe153bb82eb879d0c
rl544 <111494264+rl544@users.noreply.github.com>
2022-12-08 14:29:39 +0900

    uploaded at 2022. 12. 8. 오후 2:29:39

---
+++ programmers/문자열 나누기/solution.java
@@ -0,0 +1,32 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/140108
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        int len = s.length();
+        char rec = s.charAt(0);
+        int leftCount = 0;
+        int rightCount = 0;
+        for (int i = 0; i < len; i++){
+            if (rec == s.charAt(i)){
+                leftCount++;                
+                if (i == len-1) answer++;
+                if (i == len-1 && rightCount == 0 && answer == 0) answer = 1;
+            }
+            else {
+                rightCount++;
+                if(leftCount == rightCount){
+                    answer++;
+                    if(i < len-1){
+                        rec = s.charAt(i+1);
+                        leftCount = 0;
+                        rightCount = 0;
+                    }
+                } else if (i == len - 1){
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}
