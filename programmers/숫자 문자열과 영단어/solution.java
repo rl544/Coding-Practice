622fb01043698b15a220d18da476de866b54f75c
rl544 <111494264+rl544@users.noreply.github.com>
2022-12-17 04:45:04 +0900

    updated at 2022. 12. 17. 오전 4:45:02

--- programmers/숫자 문자열과 영단어/solution.java
+++ programmers/숫자 문자열과 영단어/solution.java
@@ -1,11 +1,10 @@
-// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/81301?language=java
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/81301
 
 class Solution {
     public int solution(String s) {
-        int[] na = {0,1,2,3,4,5,6,7,8,9};
         String[] sa = {"zero","one","two","three","four","five","six","seven","eight","nine"};
         for(int a = 0; a < 10; a++){
-            s = s.replaceAll(sa[a],na[a]+"");
+            s = s.replaceAll(sa[a],a+"");
         }
         return Integer.valueOf(s);
     }
