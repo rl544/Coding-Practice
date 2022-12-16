1939042e249b5829835367fa6d230822d83778bd
rl544 <111494264+rl544@users.noreply.github.com>
2022-12-17 04:32:20 +0900

    uploaded at 2022. 12. 17. 오전 4:32:19

---
+++ programmers/문자열 내 마음대로 정렬하기/solution.java
@@ -0,0 +1,18 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12915
+
+import java.util.Arrays;
+
+class Solution {
+  public String[] solution(String[] strings, int n) {
+      Arrays.sort(strings, new Comparator<String>(){
+          @Override
+          public int compare(String s1, String s2){
+              if(s1.charAt(n) > s2.charAt(n)) return 1;
+              else if(s1.charAt(n) == s2.charAt(n)) return s1.compareTo(s2);
+              else if(s1.charAt(n) < s2.charAt(n)) return -1;
+              else return 0;
+          }
+      });
+      return strings;
+  }
+}
