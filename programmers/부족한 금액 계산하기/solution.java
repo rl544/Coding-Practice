9f933e0c90ea8bf5bb8d92e2ffb1666d7ced5084
rl544 <111494264+rl544@users.noreply.github.com>
2022-12-17 20:05:07 +0900

    updated at 2022. 12. 17. 오후 8:05:07

--- programmers/부족한 금액 계산하기/solution.java
+++ programmers/부족한 금액 계산하기/solution.java
@@ -3,7 +3,11 @@
 import java.math.*;
 import java.util.*;
 class Solution {
-    public long solution(long price, int money, int count) {
-        return Math.max(price*(count*(count+1))/2-money,0);
+    public long solution(int price, int money, int count) {
+        BigDecimal line = new BigDecimal("0");
+        BigDecimal b2 = new BigDecimal("2");
+        line = new BigDecimal(count).multiply(new BigDecimal(count).add(new BigDecimal(1))).divide(b2);
+        System.out.println(line);
+        return (price*line.longValue()>money) ? (long)(price*line.longValue()-money) : 0;
     }
 }
