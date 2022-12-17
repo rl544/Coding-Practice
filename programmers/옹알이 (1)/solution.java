b3a0e83649b598304fd94a08c1fadc71353dd731
rl544 <111494264+rl544@users.noreply.github.com>
2022-12-18 01:48:32 +0900

    updated at 2022. 12. 18. 오전 1:48:32

--- programmers/옹알이 (1)/solution.java
+++ programmers/옹알이 (1)/solution.java
@@ -1,20 +1,10 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/120956#
 
-import java.util.*;
-import java.util.stream.IntStream;
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
-        String[] able = {"aya","ye","woo","ma"};
-        int[] ablei = {2,3,4,5,6,7,8,10};
         for(String b : babbling){
-            int blen = b.length();
-            if(IntStream.of(ablei).anyMatch(x -> x == blen)){
-                for(String c : able){
-                  b = b.replaceFirst(c,"1");
-                }
-                if(b.matches("1+")) answer += 1;
-            } else continue;
+           if(b.matches("^(aya(?!aya)|ye(?!ye)|woo(?!woo)|ma(?!ma))+$")) answer ++;
         }
         return answer;
     }
