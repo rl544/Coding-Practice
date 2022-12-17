b7c574e8784608d7beb88fe7e9fac697ebdfdcac
rl544 <111494264+rl544@users.noreply.github.com>
2022-12-18 02:30:52 +0900

    updated at 2022. 12. 18. 오전 2:30:52

--- programmers/JadenCase 문자열 만들기/solution.java
+++ programmers/JadenCase 문자열 만들기/solution.java
@@ -2,11 +2,10 @@
 
 class Solution {
     public String solution(String s) {
-        char[] ss = s.toCharArray();
+        char[] ss = s.toLowerCase().toCharArray();
         int index = 0;
         for(int i = 0; i < ss.length; i++){
             if(i==0||index==0&&ss[i]!=' ') ss[i] = Character.toUpperCase(ss[i]);
-            else ss[i] = Character.toLowerCase(ss[i]);
             if(ss[i]==' ') index=0;
             else index++;
         }
