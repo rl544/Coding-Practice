518899b57f2757a73cc1bdfecbcb00aa2770516e
rl544 <111494264+rl544@users.noreply.github.com>
2022-12-17 18:45:02 +0900

    uploaded at 2022. 12. 17. 오후 6:45:00

---
+++ programmers/같은 숫자는 싫어/solution.java
@@ -0,0 +1,16 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12906#
+
+import java.util.*;
+public class Solution {
+    public int[] solution(int []arr) {
+        Queue<Integer> q = new LinkedList<Integer>();
+        for(int b : arr){
+            Object peek = ((LinkedList<Integer>)q).peekLast();
+            if(peek != null && b == (int)peek) continue;
+            else {
+                q.add(b);
+            }
+        }//list 상태에서도 가능하지만 array가 수요이니 array로 변환합니다.
+        return q.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
