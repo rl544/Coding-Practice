b58cdaf0c66300f16f3ffa6cd89957fd5675f32e
rl544 <111494264+rl544@users.noreply.github.com>
2022-12-17 19:20:02 +0900

    uploaded at 2022. 12. 17. 오후 7:20:01

---
+++ programmers/행렬의 덧셈/solution.java
@@ -0,0 +1,13 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12950
+
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = arr1;
+        for(int a = 0; a < arr1.length; a++){
+            for(int b = 0; b < arr1[0].length; b++){
+                answer[a][b]=arr1[a][b]+arr2[a][b];
+            }
+        }
+        return answer;
+    }
+}
