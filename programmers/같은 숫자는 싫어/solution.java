2d28f047d94224e6de5dc033aee967c1ab42d233
rl544 <111494264+rl544@users.noreply.github.com>
2022-12-17 18:45:12 +0900

    updated at 2022. 12. 17. 오후 6:45:11

--- programmers/같은 숫자는 싫어/solution.java
+++ programmers/같은 숫자는 싫어/solution.java
@@ -7,9 +7,7 @@ public class Solution {
         for(int b : arr){
             Object peek = ((LinkedList<Integer>)q).peekLast();
             if(peek != null && b == (int)peek) continue;
-            else {
-                q.add(b);
-            }
+            else q.add(b);
         }//list 상태에서도 가능하지만 array가 수요이니 array로 변환합니다.
         return q.stream().mapToInt(Integer::intValue).toArray();
     }
