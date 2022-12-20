4d2246dee25594154b92a9611fcd9911efc961bf
rl544 <111494264+rl544@users.noreply.github.com>
2022-12-20 23:33:54 +0900

    updated at 2022. 12. 20. 오후 11:33:55

--- programmers/카펫/solution.java
+++ programmers/카펫/solution.java
@@ -2,17 +2,8 @@
 
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = {};
-        for(int f = 3; f < 2500; f++){
-            for(int g = 1; g < 500000000; g++){
-                if(f<g) break;
-                else if(brown == 2*f + 2*g - 4 && yellow == f*g - 2*f - 2* g + 4) {
-                    answer = new int[] {f,g};
-                    break;
-                }
-            }
-            if(answer.length > 0) break;
-        }
-        return answer;
+        int a = (brown + 4)/2; //x+y
+        int b = brown + yellow; // xy
+        return new int[] {(int) (Math.sqrt(a*a-4*b)+a)/2, (int)(a-(Math.sqrt(a*a-4*b)+a)/2)};
     }
 }
