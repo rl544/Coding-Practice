0c45fe78d81d101c4911efa6bdd149b2740fc7c4
rl544 <111494264+rl544@users.noreply.github.com>
2022-12-25 01:55:31 +0900

    updated at 2022. 12. 25. 오전 1:55:30

--- programmers/예상 대진표/solution.java
+++ programmers/예상 대진표/solution.java
@@ -1,24 +1,7 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12985
 
-class Solution
-{
-    static double baseLog(double x, double base) {
-        return Math.log10(x) / Math.log10(base);
-    }
-    private int next(int a){
-        return (a+1)/2;
-    }
-    public int solution(int n, int a, int b)
-    {
-        int answer = 0;
-        System.out.println(a+" "+b);
-        for(int c = 1; c<= baseLog(n,2); c++){
-            answer = c;
-            System.out.println(a+" "+b);
-            if((int)Math.abs(a-b)==1 && Math.min(a,b)%2==1) break;
-            a = next(a);
-            b = next(b);
-        }
-        return answer;
+class Solution{
+    public int solution(int n, int a, int b){
+        return Integer.toBinaryString((a-1)^(b-1)).length();
     }
 }
