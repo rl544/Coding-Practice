04b14272efcf43bbddd1da8d156a3bb83d90f844
rl544 <111494264+rl544@users.noreply.github.com>
2022-12-27 08:45:22 +0900

    uploaded at 2022. 12. 27. 오전 8:45:21

---
+++ programmers/전화번호 목록/solution.java
@@ -0,0 +1,21 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/42577
+
+import java.util.Arrays;
+class Solution {
+    public boolean solution(String[] pb) {
+        boolean answer = true;
+        Arrays.sort(pb);
+        for(int a = 0; a < pb.length; a++){
+            for(int b = a+1; b < a+2; b++){
+                if(pb[b%pb.length].replaceAll(pb[a],"s").matches("^s.+")){
+                    answer = false;
+                    break;
+                }
+            }
+            if(answer == false){
+                break;
+            }
+        }
+        return answer;
+    }
+}
