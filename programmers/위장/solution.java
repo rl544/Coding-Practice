a72d7e82af65f888a8757e89fb4b469cba1f4506
rl544 <111494264+rl544@users.noreply.github.com>
2022-12-27 07:50:32 +0900

    uploaded at 2022. 12. 27. 오전 7:50:31

---
+++ programmers/위장/solution.java
@@ -0,0 +1,18 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/42578
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.ArrayList;
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String,Integer> hss = new HashMap<>();
+        for(String[] sett : clothes){
+            hss.put(sett[1],hss.getOrDefault(sett[1],0)+1);
+       }
+        for(String key : hss.keySet()){
+            answer*=(hss.get(key)+1);
+        }
+        return answer-1;
+    }
+}
