7bed188929b109f508511a94a988da734a88acf3
rl544 <111494264+rl544@users.noreply.github.com>
2022-12-27 13:48:55 +0900

    uploaded at 2022. 12. 27. 오후 1:48:54

---
+++ programmers/스킬트리/solution.java
@@ -0,0 +1,28 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/49993
+
+import java.lang.StringBuilder;
+class Solution {
+    public int solution(String skill, String[] skill_trees) {
+        int answer = 0;
+        StringBuilder sb = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        StringBuilder sb3 = new StringBuilder();
+        String[] sa = new String[skill.length()];
+        sb3.append("[^(");
+        
+            for(int bb = 0; bb < skill.length(); bb++){
+            char a = skill.charAt(bb);
+            sb.append(a);
+               
+                   sb3.append(a);
+                   sb3.append((bb==skill.length()-1)?"]":"|");
+                   
+            }
+        for(String s : skill_trees){
+            String temp = s.replaceAll(sb3.toString(),"");
+            System.out.println(sb.toString()+" "+temp);
+            if(sb.toString().matches("^"+temp+".*")) answer ++;
+            }
+        return answer;
+    }
+}
