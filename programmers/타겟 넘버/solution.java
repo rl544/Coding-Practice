47b9c168dd0e9407331714a749593805c1bf9d91
rl544 <111494264+rl544@users.noreply.github.com>
2022-12-28 18:58:49 +0900

    uploaded at 2022. 12. 28. 오후 6:58:48

---
+++ programmers/타겟 넘버/solution.java
@@ -0,0 +1,23 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/43165
+
+import java.util.List;
+import java.util.ArrayList;
+class Solution {
+    
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+        List<Integer> leaves = List.of(0);
+        for(int num : numbers){
+            List<Integer> tmp = new ArrayList<>();
+            for(int parent : leaves){
+                tmp.add(parent + num);
+                tmp.add(parent - num);
+            }
+            leaves = tmp;
+        }
+        for(int leaf : leaves){
+            if(leaf==target) answer ++;
+        }
+        return answer;
+    }
+}
