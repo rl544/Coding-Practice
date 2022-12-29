50fc8cc326543be45313106604cd84fe3c48ce4e
rl544 <111494264+rl544@users.noreply.github.com>
2022-12-30 00:43:31 +0900

    uploaded at 2022. 12. 30. 오전 12:43:30

---
+++ programmers/마법의 엘리베이터/solution.java
@@ -0,0 +1,19 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/148653
+
+class Solution {
+    public int solution(int storey) {
+        int answer = 0;
+        while (storey != 0) {
+            int upperNumber = (storey %100)/10;
+            int number = storey % 10;
+            if (number > 5 || number == 5 && upperNumber>=5) {
+                storey += 10;
+                answer += (10 - number);
+            } else {
+                answer += number;
+            }
+            storey = storey / 10;
+        }  
+        return answer;
+    }
+}
