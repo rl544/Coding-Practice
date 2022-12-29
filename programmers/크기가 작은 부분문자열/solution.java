acf68490f8651184372fee850bd8efef53a2836e
rl544 <111494264+rl544@users.noreply.github.com>
2022-12-30 03:50:13 +0900

    updated at 2022. 12. 30. 오전 3:50:13

--- programmers/크기가 작은 부분문자열/solution.java
+++ programmers/크기가 작은 부분문자열/solution.java
@@ -1,16 +1,16 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/147355
 
-import java.lang.StringBuilder;
+import java.util.stream.LongStream;
+
 class Solution {
     public int solution(String t, String p) {
-        StringBuilder sb = new StringBuilder();
-        int answer = 0, len = p.length();
-        long ip = Long.parseLong(p);
-        for(int a = 0; a < t.length()-len+1; a++){
-            for(int b = a; b < a+len; b++) sb.append(t.charAt(b));
-            if(Long.parseLong(sb.toString())<=ip) answer++;
-            sb.setLength(0);
-        }
-        return answer;
+        long targetNumber = Long.parseLong(p);
+        int targetNumberLength = p.length();
+
+        return (int) LongStream.range(0L, t.length() - targetNumberLength + 1L)
+                .mapToObj(i -> t.substring((int) i, (int) i + targetNumberLength))
+                .mapToLong(Long::parseLong)
+                .filter(number -> number <= targetNumber)
+                .count();
     }
 }
