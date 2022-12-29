d455082288f258351bfaa57669be9e40fc17cacb
rl544 <111494264+rl544@users.noreply.github.com>
2022-12-30 03:55:32 +0900

    uploaded at 2022. 12. 30. 오전 3:55:32

---
+++ programmers/세균 증식/solution.java
@@ -0,0 +1,7 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/120910
+
+class Solution {
+    public int solution(int n, int t) {
+        return n << t;//bits 연산 좌항으로 옮김
+    }
+}
