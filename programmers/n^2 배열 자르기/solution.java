d96fa8098aad31c1fe4e2cf04ec8aec60ca7ce7f
rl544 <111494264+rl544@users.noreply.github.com>
2022-12-30 16:27:16 +0900

    uploaded at 2022. 12. 30. 오후 4:27:15

---
+++ programmers/n^2 배열 자르기/solution.java
@@ -0,0 +1,16 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/87390
+
+import java.util.Arrays;
+class Solution {
+    public long[] solution(int n, long left, long right) {
+        long k = left/n, l = left%n, kn = (right)/n, ln = (right)%n;
+        long[] temp = new long[(int)(right-left+1)];
+        for(long i = k; i <= kn; i++){
+            for(long j=0; j< n; j++){
+                if(i==k&&j<l||i==kn&&j>ln) continue;
+                else temp[(int)((i-k)*n+j-l)]=(Math.max(i,j)+1);
+            }
+        }
+        return temp;
+    }
+}
