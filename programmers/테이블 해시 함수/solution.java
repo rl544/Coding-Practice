fa9162754ca86d6833c13cca00ccb0d8d66ede36
rl544 <111494264+rl544@users.noreply.github.com>
2022-12-30 19:02:21 +0900

    updated at 2022. 12. 30. 오후 7:02:22

--- programmers/테이블 해시 함수/solution.java
+++ programmers/테이블 해시 함수/solution.java
@@ -7,8 +7,7 @@ class Solution {
     public int solution(int[][] data, int col, int row_begin, int row_end) {
         int answer = 0;
         List<int[]> al = Arrays.asList(data);
-        al.sort((o1,o2) -> o2[0] - o1[0]);
-        al.sort((o1,o2) -> o1[col-1] - o2[col-1]);
+        al.sort((o1,o2) -> (o1[col-1]==o2[col-1])? o2[0] - o1[0] : o1[col-1] - o2[col-1]);
         for(int c = row_begin; c <= row_end; c++) answer ^= S_i(al.get(c-1),c);
         return answer;
     }
