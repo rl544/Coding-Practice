a28b8d9d26fca289f8d0327e2b5d12ff9e99f516
rl544 <111494264+rl544@users.noreply.github.com>
2022-12-30 16:29:18 +0900

    updated at 2022. 12. 30. 오후 4:29:19

--- programmers/n^2 배열 자르기/solution.java
+++ programmers/n^2 배열 자르기/solution.java
@@ -1,15 +1,8 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/87390
 
+import java.util.stream.LongStream;
 class Solution {
-    public long[] solution(int n, long left, long right) {
-        long k = left/n, l = left%n, kn = (right)/n, ln = (right)%n;
-        long[] temp = new long[(int)(right-left+1)];
-        for(long i = k; i <= kn; i++){
-            for(long j=0; j< n; j++){
-                if(i==k&&j<l||i==kn&&j>ln) continue;
-                else temp[(int)((i-k)*n+j-l)]=(Math.max(i,j)+1);
-            }
-        }
-        return temp;
+    public int[] solution(int n, long left, long right) {
+        return LongStream.rangeClosed(left, right).mapToInt(value -> (int) (Math.max(value / n, value % n) + 1)).toArray();
     }
 }
