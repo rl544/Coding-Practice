4e363b0999f49672d839136eb611109195bc733a
rl544 <111494264+rl544@users.noreply.github.com>
2023-01-21 14:50:54 +0900

    updated at 2023. 1. 21. 오후 2:50:54

--- programmers/2 x n 타일링/solution.java
+++ programmers/2 x n 타일링/solution.java
@@ -5,9 +5,8 @@ class Solution {
         int[] ans = new int[n+1];
         for(int a = 0; a <= n; a++){
             int tmp = 0;
-            if(a == 0) tmp = 1;
-            if(a >= 1) tmp += ans[a-1];
-            if(a >= 2) tmp += ans[a-2];
+            if(a <= 1) tmp = 1;
+            if(a >= 2) tmp += ans[a-2]+ans[a-1];
             ans[a] = tmp%1000000007;
         }
         return ans[n];
